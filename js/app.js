4093df4b4c761f77ca6da0faaeb1839105879742
abled007 <audreybledsoe3@gmail.com>
2023-05-02 22:15:41 -0700

    comments

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/* Design for the game
+
+Classes
+- board 
+- player
+- pieces (king, queen, pawn, rook, knight, bishop)
+- moves
+- game status
+
+database MySQL stores user details including name, score, and ranking
+
+*/
